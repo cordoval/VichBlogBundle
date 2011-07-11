20e10096ccc0f1d6f05375c0af1b86ae45aeb17e
Dustin Dobervich <ddobervich@gmail.com>
2011-07-11 17:08:38 -0500

    implemented Tag and Category form handlers

--- Form/CategoryFormHandler.php
+++ Form/CategoryFormHandler.php
@@ -2,6 +2,10 @@
 
 namespace Vich\BlogBundle\Form;
 
+use Vich\BlogBundle\Model\CategoryManagerInterface;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  * CategoryFormHandler.
  *
@@ -9,5 +13,41 @@ namespace Vich\BlogBundle\Form;
  */
 class CategoryFormHandler
 {
+    /**
+     * @var Vich\BlogBundle\Model\CategoryManagerInterface $categoryManager
+     */
+    private $categoryManager;
     
+    /**
+     * Constructs a new instance of CategoryFormHandler.
+     * 
+     * @param Vich\BlogBundle\Model\CategoryManagerInterface $categoryManager The category manager
+     */
+    public function __construct(CategoryManagerInterface $categoryManager)
+    {
+        $this->categoryManager = $categoryManager;
+    }
+    
+    /**
+     * Processes the form.
+     * 
+     * @param Symfony\Component\HttpFoundation\Request $request The request
+     * @param Symfony\Component\Form\Form $form The form
+     * @return boolean True if the form processed successfully, false otherwise
+     */
+    public function processForm(Request $request, Form $form)
+    {
+        if ('POST' === $request->getMethod()) {
+            $form->bindRequest($request);
+            if ($form->isValid()) {
+                $cat = $form->getData();
+                
+                $this->categoryManager->updateCategory($cat);
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
 }
--- Form/TagFormHandler.php
+++ Form/TagFormHandler.php
@@ -2,6 +2,10 @@
 
 namespace Vich\BlogBundle\Form;
 
+use Vich\BlogBundle\Model\TagManagerInterface;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  * TagFormHandler.
  *
@@ -9,5 +13,41 @@ namespace Vich\BlogBundle\Form;
  */
 class TagFormHandler
 {
+    /**
+     * @var Vich\BlogBundle\Model\TagManagerInterface $tagManager
+     */
+    private $tagManager;
     
+    /**
+     * Constructs a new instance of TagFormHandler.
+     * 
+     * @param Vich\BlogBundle\Model\TagManagerInterface $tagManager The tag manager
+     */
+    public function __construct(CategoryManagerInterface $tagManager)
+    {
+        $this->tagManager = $tagManager;
+    }
+    
+    /**
+     * Processes the form.
+     * 
+     * @param Symfony\Component\HttpFoundation\Request $request The request
+     * @param Symfony\Component\Form\Form $form The form
+     * @return boolean True if the form processed successfully, false otherwise
+     */
+    public function processForm(Request $request, Form $form)
+    {
+        if ('POST' === $request->getMethod()) {
+            $form->bindRequest($request);
+            if ($form->isValid()) {
+                $tag = $form->getData();
+                
+                $this->tagManager->updateTag($tag);
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
 }
