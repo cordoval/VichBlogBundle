c467c454e9bae08a8db6bf0e27bc5cf2c950ed1f
Dustin Dobervich <ddobervich@gmail.com>
2011-07-11 15:42:46 -0500

    updated Configuration class

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,9 +19,32 @@ class Configuration
     public function getConfigTree()
     {
         $tb = new TreeBuilder();
-        $root = $tb->root('vich_blog');
-        
-        
+        $root = $tb->root('vich_blog')
+            ->children()
+                ->scalarNode('db_driver')->cannotBeOverwritten()->isRequired()->end()
+                ->arrayNode('class')
+                    ->isRequired()
+                    ->children()
+                        ->scalarNode('post')->isRequired()->end()
+                        ->scalarNode('tag')->isRequired()->end()
+                        ->scalarNode('category')->isRequired()->end()
+                    ->end()
+                ->end()
+                ->arrayNode('service')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('manager')
+                            ->addDefaultsIfNotSet()
+                            ->children()
+                                ->scalarNode('post')->cannotBeEmpty()->defaultValue('vich_blog.manager.post.default')->end()
+                                ->scalarNode('tag')->cannotBeEmpty()->defaultValue('vich_blog.manager.tag.default')->end()
+                                ->scalarNode('category')->cannotBeEmpty()->defaultValue('vich_blog.manager.category.default')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
         
         return $tb->buildTree();
     }
