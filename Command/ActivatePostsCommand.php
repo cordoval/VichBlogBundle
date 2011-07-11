70446973723ad5e625d1bb477e42e1b901ecd264
Dustin Dobervich <ddobervich@gmail.com>
2011-07-11 17:16:37 -0500

    added ActivatePostsCommand skeleton

---
+++ Command/ActivatePostsCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Vich\BlogBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+/**
+ * ActivatePostsCommand.
+ * 
+ * @author Dustin Dobervich <ddobervich@gmail.com>
+ */
+class ActivatePostsCommand extends ContainerAwareCommand
+{
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        
+    }
+    
+    /**
+     * @see Command
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        
+    }
+}
